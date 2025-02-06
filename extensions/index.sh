ccba8ea7710f2181b1e315c471896a3ed0e6e211
Clayton-Noyes <20774670+Clayton-Noyes@users.noreply.github.com>
2025-02-05 18:31:59 -0700

    extensions index updated to properly references shell files when being included from main.sh

---
+++ extensions/index.sh
@@ -0,0 +1,10 @@
+# Here you can add your own shell extensions, or if you want to follow the patterns of this repository you can just add more .sh files here in extensions and depend on the following code to catch your new shell configurations
+## PLEASE NOTE THAT IF YOU CONFIGURE YOUR PATH VARIABLES IN THIS EXTENSIONS FOLDER IT WILL OVERWRITE THE PATH CONFIGURATION FROM THIS REPOSITORY
+
+# Just add a new sh file locally to this extensions folder and the following for loop will add them and run the source command on each of them insuring that they are present in your shell
+for file in user_shells/*.sh; do
+  if [ -f "$file" ] && [ "$(basename "$file")" != "index.sh" ]; then
+    echo "Sourcing $file"
+    source "$file"
+  fi
+done
