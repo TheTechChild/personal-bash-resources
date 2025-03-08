b2b46267aa710e351b8563096105b379814aa917
Clayton-Noyes <20774670+Clayton-Noyes@users.noreply.github.com>
2025-03-07 18:33:27 -0700

    Change the source pipeline for main.sh so that custom shell changes added by the individual user is loaded last, thereby ensuring their changes persist after the shell is initialized

--- main.sh
+++ main.sh
@@ -3,13 +3,13 @@
 # Determine the directory of this main.sh file
 PBR_DIR="$HOME/personal-bash-resources"
 
-# Include any custom shell extensions added by the individual user
-source "$PBR_DIR/extensions/index.sh"
-
 # Import different bash modules from this repository
 source "$PBR_DIR/env_setup.sh"
 source "$PBR_DIR/development_resources.sh"
 source "$PBR_DIR/gaming_utilities.sh"
 source "$PBR_DIR/file_utilities.sh"
 source "$PBR_DIR/media_utilities.sh"
+source "$PBR_DIR/extensions/index.sh"
+
+# Include any custom shell extensions added by the individual user
 source "$PBR_DIR/extensions/index.sh"
